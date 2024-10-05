ce9e6a0775e84406f7b7327b863ec35dff69261e
Venila Priya <priyagetlucky1011@gmail.com>
2024-10-05 21:32:11 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SearchAndSortingBar from './components/SearchAndSortingBar';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchAndSortingBar />
+    </div>
+  );
+}
+
+export default App;
